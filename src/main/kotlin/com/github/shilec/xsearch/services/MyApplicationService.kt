994fbc05b48b479ff94d8ac40a487dec43aeb0f3
GitHub Action <action@github.com>
2022-11-07 03:02:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/shilec/xsearch/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/shilec/xsearch/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.shilec.xsearch.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.shilec.xsearch.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/shilec/xsearch/services/MyProjectService.kt
+++ src/main/kotlin/com/github/shilec/xsearch/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.shilec.xsearch.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.shilec.xsearch.MyBundle
 
 class MyProjectService(project: Project) {
 
